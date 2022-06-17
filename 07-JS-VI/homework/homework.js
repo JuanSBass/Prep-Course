3177b2971a90de9bb6baa08dfd4d9a3e3dcc9973
JuanSBass <juan_santillan20@hotmail.com>
2022-06-17 11:43:13 -0500

    bloq 6 OK

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -17,6 +17,7 @@ function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return cb(n1, n2);
 }
 
 function sumarArray(numeros, cb) {
@@ -24,12 +25,16 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  var sumnums = numeros.reduce(function(acc, elemento) { return acc + elemento});
+  cb(sumnums);
+  
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  array.forEach(function(elemento){return cb(elemento)} );
 }
 
 function map(array, cb) {
@@ -37,12 +42,16 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  var newarray = array.map(function(x){return cb(x)})
+  return newarray;
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  var filtro = array.filter(array => array[0] === "a");
+  return filtro;
 }
 
 // No modificar nada debajo de esta línea
